a1812cd5adc5a88aba46a13dbfc828ca7d2c0740
Meghna-Saini <meghna.ug23@nsut.ac.in>
2026-06-12 17:36:56 +0530

    Making the base for my project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/home/home'
+import Cart from './pages/cart/cart'
+import PlaceOrder from './pages/placeOrder/placeOrder'
 
 const App = () => {
   return (
-    <div clasName='app'>
+    <div className='app'>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/place-order' element={<PlaceOrder/>}/>
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/pages/home/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './home.css'
+const home = () => {
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
+
+export default home
+

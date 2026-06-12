8f7166de048a600d75de7791cde74a06396e467f
Meghna-Saini <meghna.ug23@nsut.ac.in>
2026-06-12 20:51:42 +0530

    Some changes in Homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/home/home'
-import Cart from './pages/cart/cart'
-import PlaceOrder from './pages/placeOrder/placeOrder'
+import { Route, Routes } from 'react-router-dom'
+import home from './pages/home/home'
+import cart from './pages/cart/cart'
+import placeOrder from './pages/placeOrder/placeOrder'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/place-order' element={<PlaceOrder/>}/>
+        <Route path='/' element={<home/>}/>
+        <Route path='/cart' element={<cart/>}/>
+        <Route path='/Order' element={<placeOrder/>}/>
       </Routes>
     </div>
   )

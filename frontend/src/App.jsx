23c48758e8bd55f55c29f2e83415ac41bf063015
Meghna-Saini <meghna.ug23@nsut.ac.in>
2026-06-15 21:59:21 +0530

    added Cart

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,19 @@ import Home from './pages/home/home'
 import Cart from './pages/cart/cart'
 import PlaceOrder from './pages/placeOrder/placeOrder'
 import Footer from './components/Footer/Footer'
+import LoginPopup from './components/LoginPopup/LoginPopup'
+import { useState } from 'react'
+
 const App = () => {
+
+  const [showLogin, setShowLogin] = useState(false);
+
+
   return (
     <>
+    {showLogin? <LoginPopup setShowLogin = {setShowLogin}/>:<></>}
     <div className='app'>
-      <Navbar/>
+      <Navbar setShowLogin={setShowLogin}/>
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/cart' element={<Cart/>}/>

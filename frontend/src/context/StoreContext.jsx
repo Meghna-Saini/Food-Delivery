9be0ab35acaa7f5887134bc59b004fd1b91a1cfa
Meghna-Saini <meghna.ug23@nsut.ac.in>
2026-06-16 12:39:43 +0530

    Frontend Completed

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -21,7 +21,7 @@ const removeFromCart = (itemId) => {
 
    const getTotalCartAmount = () => {
         let totalAmount = 0;
-        for(constitem in cartItems){
+        for(const item in cartItems){
                 if(cartItems[item]>0){
                    let itemInfo = food_list.find((product)=>product._id===item);
                 totalAmount += itemInfo.price*cartItems[item];
--- frontend/src/pages/cart/cart.jsx
+++ frontend/src/pages/cart/cart.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import './cart.css'
 import { useContext } from 'react'
 import { StoreContext} from '../../context/StoreContext'
+import { useNavigate } from 'react-router-dom';
+
 const Cart = () => {
 
   const {cartItems,food_list,removeFromCart,getTotalCartAmount} = useContext(StoreContext);
+
+  const navigate = useNavigate();
 
   return (
     <div className='cart'>
@@ -43,20 +47,20 @@ const Cart = () => {
           <div className="">
             <div className="cart-total-details">
                 <p>Subtotal</p>
-                <p>{getTotalCartAmount()}</p>
+                <p>${getTotalCartAmount()}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>{2}</p>
+              <p>${getTotalCartAmount()===0?0:2}</p>
             </div>
             <hr />
             <div className="cart-total-details">
                <p>Total</p>
-               <p>{getTotalCartAmount()+2}</p>
+               <p>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</p>
             </div>   
           </div>
-           <button>PROCEED TO CHECKOUT</button>
+           <button onClick={()=>navigate('/placeOrder')}>PROCEED TO CHECKOUT</button>
         </div>
         <div className="cart-promocode">
           <div>
